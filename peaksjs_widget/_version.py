e75ceca5b2dac85c3acdba52d3c61b96f1a0112d
Antoine Daurat <antoinedaurat@gmail.com>
2023-04-10 06:20:07 +0200

    Bump to 0.0.1

--- peaksjs_widget/_version.py
+++ peaksjs_widget/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) AntoineDaurat.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 0, 5)
+version_info = (0, 0, 1)
 __version__ = ".".join(map(str, version_info))
